0429835df0a1e3d1053647f88b2571903b79d30f
Lucas Martinuzzo <lucasmartinuzzo@hotmail.com>
2021-06-28 15:59:53 -0300

    Update write_txt.py

--- tools/write_txt.py
+++ tools/write_txt.py
@@ -1,8 +1,8 @@
 import os
 
 
-def getfiles():
-    filenames=os.listdir('./DATA/Train/train')
+def getfiles(path):
+    filenames=os.listdir(path)
     print(filenames)
     return filenames
 
@@ -12,11 +12,26 @@ if __name__ == '__main__':
 
     if not os.path.exists('train.txt'):
         os.mknod('train.txt')
-
-    a = getfiles()
+    training_path = './DATA/Train/train'
+    a = getfiles(training_path)
     # a.spilt('')
     l = len(a)
     with open("train.txt", "w") as f:
+        for i in range(l):
+            print(a[i])
+            x = a[i]
+            f.write(x)
+            f.write('\n')
+        f.close()
+    
+    
+    if not os.path.exists('test.txt'):
+        os.mknod('test.txt')
+    test_path = './DATA/Test/test'
+    a = getfiles(test_path)
+    # a.spilt('')
+    l = len(a)
+    with open("test.txt", "w") as f:
         for i in range(l):
             print(a[i])
             x = a[i]
